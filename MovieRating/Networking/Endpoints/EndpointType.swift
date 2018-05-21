9792719154681b8bc8067f7896f81d3e286e4117
HienQTran <hien.trquang@gmail.com>
2018-05-20 19:37:12 -0500

    Code Refactor

--- MovieRating/Networking/Endpoints/EndpointType.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EndpointType.swift
-//  MovieRating
-//
-//  Created by Hien Quang Tran on 19/5/18.
-//  Copyright © 2018 Hien Tran. All rights reserved.
-//
-
-import Foundation
--- MovieRating/Networking/Services/HTTPMethod.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HTTPMethod.swift
-//  MovieRating
-//
-//  Created by Hien Quang Tran on 19/5/18.
-//  Copyright © 2018 Hien Tran. All rights reserved.
-//
-
-import Foundation
