38384ecd12d2d418f63361f2db1cc8026b7d865d
HienQTran <hien.trquang@gmail.com>
2018-06-01 20:11:28 -0500

    update playground to switch between different languages

--- MovieRatingPlaygound.playground/Pages/WalkthroughPageViewControllerPlayground.xcplaygroundpage/Contents.swift
+++ MovieRatingPlaygound.playground/Pages/WalkthroughPageViewControllerPlayground.xcplaygroundpage/Contents.swift
@@ -4,11 +4,14 @@ import UIKit
 import MovieRatingFramework
 import PlaygroundSupport
 
-let walkthroughViewModel = WalkthroughViewModel()
+AppConfigurationManager.updateCurrentConfig(language: .ja)
 
+let walkthroughViewModel = WalkthroughViewModel()
 let walkthroughPageViewController = WalkthroughPageViewController(viewModel: walkthroughViewModel)
 
-let (parentVC, _) = traitControllers(device: .pad,
+
+
+let (parentVC, _) = traitControllers(device: .phone5_5inch,
                                      orientation: .portrait,
                                      child: walkthroughPageViewController)
 
